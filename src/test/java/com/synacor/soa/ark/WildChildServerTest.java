9fa13684d924d10d894eb21908ced2024ed85f10
brian <brian.decamp@gorillalogic.com>
2013-04-22 11:16:44 -0600

    Single test class

--- src/test/java/com/synacor/soa/ark/WildChildServerTest.java
+++ src/test/java/com/synacor/soa/ark/WildChildServerTest.java
@@ -1,10 +1,13 @@
 package com.synacor.soa.ark;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher.Event.EventType;
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.netflix.curator.framework.CuratorFramework;
@@ -16,6 +19,7 @@ import com.netflix.curator.test.Timing;
 public class WildChildServerTest {
 	private String connectString = "localhost:2181";
 	private Timing timing = new Timing();
+	private Logger log = Logger.getLogger(WildChildServerTest.class);
 	
 	public static void main(String[] argv) throws Exception {
 		new WildChildServerTest().testServer();
@@ -26,10 +30,11 @@ public class WildChildServerTest {
     	private final Set<String> created = new HashSet<String>();
     	private final Set<String> changed = new HashSet<String>();
     	private final Set<String> deleted = new HashSet<String>();
+    	private Logger log = Logger.getLogger(CountCuratorWatcher.class);
     	
         public void process(WatchedEvent event) throws Exception
         {
-        	System.out.println("LEAF EVENT: " + event.getPath() + " " + event.getType().name());
+        	log.info("LEAF EVENT: " + event.getPath() + " " + event.getType().name());
         	if(event.getType() == EventType.NodeCreated) {
         		created.add(event.getPath());
         	} else if(event.getType() == EventType.NodeDeleted) {
@@ -44,11 +49,34 @@ public class WildChildServerTest {
 	public void testServer() throws Exception {
         final CountCuratorWatcher watcher = new CountCuratorWatcher();
         Set<String> currentNodes = new WildChild(newClient(), "/services/.*/deployments/.*/instances/.*/lifecycleState", watcher).getMatchingLeaves();
-		System.out.println("Initial node count: " + currentNodes.size());
-		synchronized(this) {
-			wait();
-		}
+		log.info("Initial node count: " + currentNodes.size());
+
+		
+		CuratorFramework client = newClient();
+		deleteRecursive(client, "/services");
+		timing.sleepABit();
+		log.info("Deleted: " + watcher.deleted.size());
+
+		client.create().creatingParentsIfNeeded().forPath("/services/a/deployments/1.0.0/instances/1/lifecycleState");
+		client.create().creatingParentsIfNeeded().forPath("/services/a/deployments/1.0.0/instances/2/lifecycleState");
+		client.create().creatingParentsIfNeeded().forPath("/services/a/deployments/1.0.1/instances/1/lifecycleState");
+		client.create().creatingParentsIfNeeded().forPath("/services/a/deployments/1.0.1/instances/2/lifecycleState");
+		client.create().creatingParentsIfNeeded().forPath("/services/b/deployments/1.0.0/instances/1/lifecycleState");
+		client.create().creatingParentsIfNeeded().forPath("/services/b/deployments/1.0.0/instances/2/lifecycleState");
+
+        timing.sleepABit();
+		log.info(watcher.created.size() == 6);
+		Assert.assertTrue(watcher.changed.size() == 0);
 	}
+
+    private void deleteRecursive(CuratorFramework client, String path) throws Exception {
+    	if(client.checkExists().forPath(path) == null) return;
+    	List<String> children = client.getChildren().forPath(path);
+    	for(String child : children) {
+    		deleteRecursive(client, path + "/" + child);
+    	}
+    	client.delete().forPath(path);
+    }
 
     private CuratorFramework newClient() {
 		CuratorFramework client = CuratorFrameworkFactory
